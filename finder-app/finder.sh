01354e72a35332014c486ec92bb204419dc968f6
Kate Chamberlin <kate_chamberlin@trimble.com>
2025-05-19 16:17:10 +1200

    Add comments for peers

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,17 +1,24 @@
 #!/bin/bash -e
 
-
+# if the number of arguments is not 2, print usage and exit
 if [ $# -ne 2 ]; then
     echo "Usage: $0 <filesdir> <searchstr>"
     exit 1
+
+# If the directory does not exist, print error and exit
 elif [ ! -d "$1" ]; then
     echo "Error: Directory $1 does not exist."
     exit 1
 fi
 
-
+# rename the variables for readability
 filesdir=$1
 searchstr=$2
 
+# search for any objects that are files (type f) in the filesdir provided, and
+# print the filename, then count the number of lines(filenames) printed with wc -l
 echo "The number of files are $(find $filesdir/ -name "*.*" -type f -print | wc -l) "
+
+# search for the string recuring in the filesdir provided, and print the
+# filename, then count the number of lines(filenames) printed with wc -l
 echo "and the number of matching lines are $( grep -r $searchstr $filesdir/* | wc -l)"
--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,20 +1,22 @@
 #!/bin/bash -e
 
+# If the number of arguments is not 2, print usage and exit
 if [ $# -ne 2 ]; then
     echo "Usage: $0 <writefile> <writestr>"
     exit 1
 fi
 
-
+# get the path of the file without the filename
 path=$(dirname "$1")
-filename=$(basename "$1")
+# if the path does not exist, create it
 if [ ! -e "$path" ]; then
     mkdir -p "$path" 
 fi
 
-if [ ! -e "$1" ]; then
+# if the file does not exist, create it
+if [ ! -f "$1" ]; then
     touch "$1"
 fi
+# write the string to the file (overwriting any existing content)
 echo $2 > $1
-# echo "file $1 contains $(cat $1)"
 
